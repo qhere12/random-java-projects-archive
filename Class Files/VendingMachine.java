3a2c1aa9d2bc71077b017b0c2f7a98dd2dcd2694
tuna <75491993+qhere12@users.noreply.github.com>
2025-12-11 23:39:34 +0300

    Refactor VendingMachine class structure and logic

--- Class Files/VendingMachine.java
+++ Class Files/VendingMachine.java
@@ -1,5 +1,3 @@
-package helloapagain31;
-
 import java.util.Scanner;
 
 public class VendingMachine {
@@ -103,13 +101,5 @@ public void buy(User currentUser, Item whichItem) {
 			}
 		}
 		}
-		
-
-
 }	
 }
-
-
-
-
-
